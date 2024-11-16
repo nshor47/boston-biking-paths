6270c07ce201fdc2ad70ff0d2a5b4d6e4a754d05
nshor47 <91350878+nshor47@users.noreply.github.com>
2024-11-15 16:02:06 -0800

    Lab 8 Step 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import mapboxgl from 'mapbox-gl';
+    import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+    mapboxgl.accessToken = 'pk.eyJ1IjoibnNob3I0NyIsImEiOiJjbTNqZHpkemowMmNyMnFwdGRib3hrdHFrIn0.JyaBZZ6ZeBmeptjJhSR8nQ';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-71.0926, 42.3601],
+        zoom: 12,
+        minZoom: 5,
+        maxZoom: 18
+    });
+    });
+</script>
+
+<style>
+@import url('$lib/global.css');
+
+</style>
+
+<body>Boston Bike Paths</body>
+<div id="map" style="width: 100%; height: 500px;"></div>
+
